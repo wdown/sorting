95dd4dbe951efbdd950480b9f806c99c947ac8f2
wdown <watered.down@gmail.com>
2013-12-11 09:28:34 -0800

    removed last pass over array

--- BubbleSort.java
+++ BubbleSort.java
@@ -7,11 +7,12 @@ public class BubbleSort {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] array = new int[] {61,60,62,64,58,66,56,59,57,60,53,55,54,50,47,
+		int[] array = new int[] {61,61,60,62,64,58,66,56,59,57,60,53,55,54,50,47,
 				46,51,49,48,44,45,43,41,40,42,35,39,31,28,33,32,37,38,23,21,22,19,15};
 		
-		
-		for (int n=0; n <(array.length); n++) {
+		// set n < array.length-1 since initial pass will push the largest
+		// to the last element anyway
+		for (int n=0; n <(array.length-1); n++) {
 			for (int i=0;i<(array.length)-1;i++){
 				if (array[i] > array[i+1]){
 					int tmp = array[i];
