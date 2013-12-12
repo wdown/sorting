19d449073a2f55b900258ac40b9c306a16b8cd75
wdown <watered.down@gmail.com>
2013-12-12 15:50:13 -0800

    non-valid quick sort

---
+++ QuickSort.java
@@ -0,0 +1,137 @@
+//import java.util.ArrayList;
+//import java.util.List;
+
+
+public class QuickSort {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] array = new int[] {61,60,62,64,58,66,56,59,59,57,60,53,55,54,50};
+		//		46,51,49,48,44,45,43,41,40,42,35,39,31,28,33,32,37,38,23,21,22,19,15};
+		
+	//	int[] array = new int[] {22,77,15,21,19};
+		
+		for (int i=0; i<array.length-1;i++){
+			if (array[i] > array[i+1]) {
+
+		int n = 0;
+		
+		int divisions = (int) Math.pow(2,n);
+		System.out.println("division is: " + divisions);
+	
+		System.out.println("array length is: " + array.length);
+		// get number of elements in each section
+		int size = (array.length)/divisions -1;
+		//System.out.println("size is: " + size);
+	
+		//set pivot point for each section
+		int pivot = (size/2)+1;
+		System.out.println("pivot is: " + array[pivot]);
+		System.out.println("");
+	
+			for (i=0; i<size; i++){
+				int tmpLeft = 0;
+				int tmpRight = 0;
+			
+				if (i<=(size/2)) {
+					System.out.println("processing i:" + i);
+					System.out.println("");
+					
+					if (array[i] > array[pivot]) {
+						//if array element is bigger than pivot, put in tmp integer
+						tmpRight = array[i];
+						System.out.println("tmpRight is now: " + array[i]);
+						//look for an i > size/2 to swap with
+						for (int j=size;j>(pivot);j--) {
+							System.out.println("j is: " + j);
+							System.out.println("array[j] is: " + array[j]);
+							for (int k : array){
+								System.out.print(k + ",");
+							}
+							
+							System.out.println("");
+							
+							System.out.println("array[j]: " + array[j] + " pivot: " + array[pivot]);
+							if (array[j] < array[pivot]) {
+								System.out.println("array[j] < array[pivot] was invoked");
+								array[i] = array[j];
+								array[j] = tmpRight;
+								for (int k : array){
+									System.out.print(k + ",");
+								}
+								System.out.println("");
+								System.out.println(array[i] + " < " + array[pivot] + " < " + array[j]);
+								break;
+							}
+							
+						
+							// if a suitable swap is not found left of pivot
+							// swap with pivot
+							//{61,60,62,64,58,66,56,59,57,60,53,55,54,50};
+							else if (j==pivot){
+								System.out.println("array[i]: " + array[i] + " pivot: " + array[pivot]);
+								tmpLeft = array[(size/2)+1];
+								array[(size/2)+1] = array[i];
+								array[i] = tmpLeft;
+								System.out.println(array[i] + " > " + array[pivot] + " = " + array[j]);
+							}
+						}	
+					}
+					
+					else if (array[i] < array[pivot]) {
+						System.out.println("NO CHANGE: array[i]: " + array[i] + " pivot: " + array[pivot]);
+						
+					}
+					else if (array[i] == array[pivot]) {
+						System.out.println("PIVOT array[i]: " + array[i] + " pivot: " + array[pivot]);
+					//	tmpArray.add(array[pivot+1]);
+					}
+					
+				}
+			// now assume all elements on the left are in place
+			// still need to clean up the right side
+				else if (i>(size/2)) {
+					System.out.println("processing i:" + i);
+					System.out.println("");
+					
+					if (array[i] < array[pivot]) {
+					//	array[i] = array[(size/2+2)];
+						System.out.println(array[i] + " is less than: " + array[pivot]);
+						
+						tmpRight = array[(size/2)+1];
+						array[(size/2)+1] = array[i];
+						array[i] = tmpRight;
+						System.out.println(array[i] + " > " + array[pivot]);
+					//	tmpArray.add(array[pivot+1]);
+					}
+					else if (array[i] > array[pivot]) {
+						System.out.println(array[i] + " is greater than: " + array[pivot]);
+						//tmpArray.add(array[pivot-1]);
+					}
+					else if (array[i] == array[pivot]) {
+						System.out.println(array[i] + " is pivot");
+					}
+				}
+				
+			}
+			n++;
+		}
+			
+			
+
+		//	System.out.println("");
+		
+	/*
+				if (j > j+1) {
+					System.out.println("you failed to sort this array");
+				}
+				else {
+				*/
+				System.out.print(i + ",");
+				
+			}
+	
+	}
+}
